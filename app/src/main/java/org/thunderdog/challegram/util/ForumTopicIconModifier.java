9121038822dcc780f19b2287f0ecb2fece682b2a
AbdulKus <73951988+AbdulKus@users.noreply.github.com>
2026-07-24 23:59:47 +0300

    Improve forum topic management

---
+++ app/src/main/java/org/thunderdog/challegram/util/ForumTopicIconModifier.java
@@ -0,0 +1,154 @@
+/*
+ * This file is a part of Telegram X
+ * Copyright © 2014 (tgx-android@pm.me)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+package org.thunderdog.challegram.util;
+
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+
+import org.drinkless.tdlib.TdApi;
+import org.thunderdog.challegram.R;
+import org.thunderdog.challegram.component.sticker.TGStickerObj;
+import org.thunderdog.challegram.loader.ComplexReceiver;
+import org.thunderdog.challegram.loader.ComplexReceiverProvider;
+import org.thunderdog.challegram.loader.ImageReceiver;
+import org.thunderdog.challegram.loader.gif.GifReceiver;
+import org.thunderdog.challegram.telegram.Tdlib;
+import org.thunderdog.challegram.telegram.TdlibEmojiManager;
+import org.thunderdog.challegram.theme.ColorId;
+import org.thunderdog.challegram.theme.Theme;
+import org.thunderdog.challegram.tool.DrawAlgorithms;
+import org.thunderdog.challegram.tool.Drawables;
+import org.thunderdog.challegram.tool.Paints;
+import org.thunderdog.challegram.tool.Screen;
+
+import java.lang.ref.WeakReference;
+
+public final class ForumTopicIconModifier implements DrawModifier, TdlibEmojiManager.Watcher {
+  private static final long IMAGE_RECEIVER_KEY = 1;
+  private static final long GIF_RECEIVER_KEY = 2;
+
+  private final Tdlib tdlib;
+  private final long customEmojiId;
+  private final int color;
+  private final Drawable forumIcon;
+  private final Drawable pinIcon;
+
+  private TGStickerObj sticker;
+  private WeakReference<View> boundView;
+  private boolean postponedRequestSent;
+  private boolean pinned;
+
+  public ForumTopicIconModifier (Tdlib tdlib, TdApi.ForumTopicIcon icon, boolean pinned) {
+    this.tdlib = tdlib;
+    this.customEmojiId = icon != null ? icon.customEmojiId : 0;
+    this.color = icon != null && icon.color != 0 ? 0xff000000 | icon.color : Theme.getColor(ColorId.icon);
+    this.pinned = pinned;
+    this.forumIcon = Drawables.get(R.drawable.baseline_forum_16);
+    this.pinIcon = Drawables.get(R.drawable.deproko_baseline_pin_14);
+    if (customEmojiId != 0) {
+      TdlibEmojiManager.Entry entry = tdlib.emoji().findOrPostponeRequest(customEmojiId, this);
+      if (entry != null && !entry.isNotFound()) {
+        setSticker(entry);
+      }
+    }
+  }
+
+  public void bind (View view) {
+    boundView = new WeakReference<>(view);
+    if (customEmojiId == 0) {
+      view.invalidate();
+      return;
+    }
+    if (sticker == null) {
+      if (!postponedRequestSent) {
+        postponedRequestSent = true;
+        tdlib.emoji().performPostponedRequests();
+      }
+      return;
+    }
+    requestFiles(view);
+    view.invalidate();
+  }
+
+  public void setPinned (boolean pinned) {
+    if (this.pinned != pinned) {
+      this.pinned = pinned;
+      View view = boundView != null ? boundView.get() : null;
+      if (view != null) {
+        view.invalidate();
+      }
+    }
+  }
+
+  private void setSticker (@NonNull TdlibEmojiManager.Entry entry) {
+    if (entry.value != null) {
+      sticker = new TGStickerObj(tdlib, entry.value, null, new TdApi.StickerTypeCustomEmoji());
+    }
+  }
+
+  private void requestFiles (View view) {
+    if (!(view instanceof ComplexReceiverProvider) || sticker == null) {
+      return;
+    }
+    ComplexReceiver receiver = ((ComplexReceiverProvider) view).getComplexReceiver();
+    receiver.getImageReceiver(IMAGE_RECEIVER_KEY).requestFile(sticker.isAnimated() ? sticker.getImage() : sticker.getFullImage());
+    receiver.getGifReceiver(GIF_RECEIVER_KEY).requestFile(sticker.getPreviewAnimation());
+  }
+
+  @Override
+  public void onCustomEmojiLoaded (TdlibEmojiManager context, TdlibEmojiManager.Entry entry) {
+    if (entry.customEmojiId != customEmojiId || entry.isNotFound()) {
+      return;
+    }
+    tdlib.ui().post(() -> {
+      setSticker(entry);
+      View view = boundView != null ? boundView.get() : null;
+      if (view != null) {
+        requestFiles(view);
+        view.invalidate();
+      }
+    });
+  }
+
+  @Override
+  public void afterDraw (View view, Canvas c) {
+    int size = Screen.dp(30f);
+    int left = Screen.dp(15f);
+    if (org.thunderdog.challegram.core.Lang.rtl()) {
+      left = view.getMeasuredWidth() - left - size;
+    }
+    int top = (view.getMeasuredHeight() - size) / 2;
+
+    c.drawCircle(left + size / 2f, top + size / 2f, size / 2f, Paints.fillingPaint(color));
+    if (sticker != null && view instanceof ComplexReceiverProvider) {
+      ComplexReceiver receiver = ((ComplexReceiverProvider) view).getComplexReceiver();
+      ImageReceiver imageReceiver = receiver.getImageReceiver(IMAGE_RECEIVER_KEY);
+      GifReceiver gifReceiver = receiver.getGifReceiver(GIF_RECEIVER_KEY);
+      float scale = sticker.getDisplayScale();
+      DrawAlgorithms.drawReceiver(c, imageReceiver, gifReceiver, false, true,
+        left, top, left + size, top + size, scale, scale);
+    } else {
+      Drawables.drawCentered(c, forumIcon, left + size / 2f, top + size / 2f, Paints.whitePorterDuffPaint());
+    }
+
+    if (pinned) {
+      float pinX = left + size - Screen.dp(2f);
+      float pinY = top + size - Screen.dp(2f);
+      c.drawCircle(pinX, pinY, Screen.dp(8f), Paints.fillingPaint(Theme.fillingColor()));
+      Drawables.drawCentered(c, pinIcon, pinX, pinY, Paints.getIconGrayPorterDuffPaint());
+    }
+  }
+}
